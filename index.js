03b0adcf63f1f7761d89f8ded7ef19a05201c8ad
joshua sorkin <joshua.d.sorkin@gmail.com>
2022-08-16 14:49:25 -0700

    add Google Speech to Text to websocket handling

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const wss = new WebSocket.Server({ server });
 const WaveFile = require('wavefile').WaveFile;
 const port = 8080;
 const path = require('path');
+
+//include Google speech to text
 const speech = require('@google-cloud/speech');
 const client = new speech.SpeechClient();
 
+//configure transcription request
 const request = {
     config: {
         encoding:"MULAW",
@@ -24,17 +27,29 @@ let chunks = [];
 //handle websocket connnection
 wss.on("connection",function connection(ws) {
     console.log("New Connection Initiated");
+
+    let recognizeStream = null;
+
     ws.on("message",function incoming(message) {
         const msg = JSON.parse(message);
         switch(msg.event){
             case "connected":
                 console.log(`A new call has connected.`);
+
+                //create stream to Google Speech to Text API
+                recognizeStream = client
+                    .streamingRecognize(request)
+                    .on("error",console.error)
+                    .on("data",data => {
+                        console.log(data.results[0].alternatives[0].transcript);
+                    })
                 break;
             case "start":
                 console.log(`Let flow the Media Stream ${msg.streamSid}`);
                 break;
             case "media":
-                console.log("Media message...");
+                //write media packets to the recognize stream
+                recognizeStream.write(msg.media.payload);
                 break;
             case "stop":
                 console.log(`Call has ended`);
